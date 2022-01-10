52ee1560e3a25e7972b3d3d87f91396647f93c1c
Kaligula <ilya.kalganov.97@mail.ru>
2022-01-10 21:38:15 +0300

    [ADD] Custom AR view

---
+++ ARPtorotype/Views/ARViews/CustomARView.swift
@@ -0,0 +1,33 @@
+//
+//  CustomARView.swift
+//  ARPtorotype
+//
+//  Created by Kaligula on 10.01.2022.
+//
+
+import RealityKit
+import ARKit
+import FocusEntity
+
+class CustomARView: ARView {
+    
+    var focusEntity: FocusEntity?
+    
+    required init(frame frameRect: CGRect) {
+        super.init(frame: frameRect)
+        
+        focusEntity = FocusEntity(on: self, focus: .classic)
+        
+        configure()
+    }
+    
+    private func configure() {
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.vertical, .horizontal]
+        session.run(config)
+    }
+    
+    @MainActor @objc required dynamic init?(coder decoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
